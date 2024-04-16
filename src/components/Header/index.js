59e74222932dd725ee9a4faee3e9d2396565b497
Manatocode <gabrielmartinellix@outlook.com>
2024-04-16 17:47:20 -0300

    ReactJS

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,21 @@ import './style.css'
 import Logo from '../Logo'
 import NavBar from '../NavBar'
 import NavBarGif from '../NavBarGif'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+background-color: white;
+display: flex;
+justify-content: center;
+}
+`
 
 function Header() {
-    return (<header className="App-header">
+    return (<HeaderContainer>
         <Logo />
         <NavBar />
         <NavBarGif />
-    </header>
+    </HeaderContainer>
     )
 }
 
