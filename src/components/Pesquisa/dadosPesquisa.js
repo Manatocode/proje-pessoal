d157b5586cb96eb768543a4061eadfb68e6561ac
Manatocode <gabrielmartinellix@outlook.com>
2024-04-18 17:49:33 -0300

    3

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/meio.jpg'
+
+export const livros = [
+    {nome:'A vida de Joana', id: 1, src:livro},
+    {nome:'Alice e Mary', id: 2, src:livro},
+    {nome:'Isa e Fenry', id: 3, src:livro},
+    {nome:'Andrique, o solitário', id: 4, src:livro},
+]
